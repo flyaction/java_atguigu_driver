5f87b647cab141b5724e1c6f70a28d3d2f967aad
action <xingdong1117@126.com>
2025-07-16 15:49:53 +0800

    first commit

---
+++ daijia-parent/service/service-customer/src/main/java/com/atguigu/daijia/customer/config/WxConfigProperties.java
@@ -0,0 +1,18 @@
+package com.atguigu.daijia.customer.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: action
+ * @create: 2025/7/16 15:41
+ **/
+@Component
+@Data
+@ConfigurationProperties(prefix = "wx.miniapp")
+public class WxConfigProperties {
+
+    private String appId;
+    private String secret;
+}
