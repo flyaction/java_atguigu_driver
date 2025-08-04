4978f37fa915393e4be3959bc0b29a8cb2e0d252
action <xingdong1117@126.com>
2025-08-04 18:00:26 +0800

    feat:腾讯云cos文件上传

---
+++ daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/config/TencentCloudProperties.java
@@ -0,0 +1,20 @@
+package com.atguigu.daijia.driver.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: action
+ * @create: 2025/8/4 17:33
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "tencent.cloud")
+public class TencentCloudProperties {
+
+    private String secretId;
+    private String secretKey;
+    private String region;
+    private String bucketPrivate;
+}
