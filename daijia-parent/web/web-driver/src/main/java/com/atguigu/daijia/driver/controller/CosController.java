1ceb0dd3b4119c6b1a54af6a142167d72076adeb
action <xingdong1117@126.com>
2025-09-03 14:13:36 +0800

    feat:修改上传图片

--- daijia-parent/web/web-driver/src/main/java/com/atguigu/daijia/driver/controller/CosController.java
+++ daijia-parent/web/web-driver/src/main/java/com/atguigu/daijia/driver/controller/CosController.java
@@ -24,10 +24,11 @@ public class CosController {
     @Operation(summary = "上传")
     //@GuiguLogin
     @PostMapping("/upload")
-    public Result<CosUploadVo> upload(@RequestPart("file") MultipartFile file,
+    public Result<String> upload(@RequestPart("file") MultipartFile file,
                                       @RequestParam(name = "path",defaultValue = "auth") String path) {
         CosUploadVo cosUploadVo = cosService.uploadFile(file,path);
-        return Result.ok(cosUploadVo);
+        String showUrl = cosUploadVo.getShowUrl();
+        return Result.ok(showUrl);
     }
 }
 
