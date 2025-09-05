1544f9520b94181d8a226cec1ad3d19bb4082297
action <xingdong1117@126.com>
2025-09-05 09:46:35 +0800

    feat:腾讯云COS图片增加审核

--- daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/service/CiService.java
+++ daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/service/CiService.java
@@ -2,5 +2,7 @@ package com.atguigu.daijia.driver.service;
 
 public interface CiService {
 
+    //图片审核
+    Boolean imageAuditing(String path);
 
 }
--- daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
+++ daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/service/impl/CosServiceImpl.java
@@ -1,6 +1,9 @@
 package com.atguigu.daijia.driver.service.impl;
 
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.driver.config.TencentCloudProperties;
+import com.atguigu.daijia.driver.service.CiService;
 import com.atguigu.daijia.driver.service.CosService;
 import com.atguigu.daijia.model.vo.driver.CosUploadVo;
 import com.qcloud.cos.COSClient;
@@ -31,6 +34,9 @@ public class CosServiceImpl implements CosService {
 
     @Autowired
     private TencentCloudProperties tencentCloudProperties;
+
+    @Autowired
+    private CiService ciService;
 
     private COSClient getCosClient() {
         COSCredentials cred = new BasicCOSCredentials(tencentCloudProperties.getSecretId(), tencentCloudProperties.getSecretKey());
@@ -82,6 +88,15 @@ public class CosServiceImpl implements CosService {
         putObjectRequest.setStorageClass(StorageClass.Standard);
         PutObjectResult putObjectResult = cosClient.putObject(putObjectRequest); //上传文件
         cosClient.shutdown();
+
+        //图片审核
+        Boolean imageAuditing = ciService.imageAuditing(uploadPath);
+        if(!imageAuditing) {
+            //删除违规图片
+            cosClient.deleteObject(tencentCloudProperties.getBucketPrivate(),uploadPath);
+            throw new GuiguException(ResultCodeEnum.IMAGE_AUDITION_FAIL);
+        }
+
 
         //返回vo对象
         CosUploadVo cosUploadVo = new CosUploadVo();
