6f02a0b7ebfa61cd4c26b4a4b941ac0cddb43520
action <xingdong1117@126.com>
2025-09-11 16:30:16 +0800

    feat:支付成功后续处理

--- daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/mapper/DriverAccountMapper.java
+++ daijia-parent/service/service-driver/src/main/java/com/atguigu/daijia/driver/mapper/DriverAccountMapper.java
@@ -3,11 +3,13 @@ package com.atguigu.daijia.driver.mapper;
 import com.atguigu.daijia.model.entity.driver.DriverAccount;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.math.BigDecimal;
 
 @Mapper
 public interface DriverAccountMapper extends BaseMapper<DriverAccount> {
 
-    void add(Long driverId, BigDecimal amount);
+    //添加奖励到司机账户表
+    void add(@Param("driverId") Long driverId, @Param("amount") BigDecimal amount);
 }
--- daijia-parent/service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxPayServiceImpl.java
+++ daijia-parent/service/service-payment/src/main/java/com/atguigu/daijia/payment/service/impl/WxPayServiceImpl.java
@@ -6,9 +6,14 @@ import com.atguigu.daijia.common.execption.GuiguException;
 import com.atguigu.daijia.common.result.ResultCodeEnum;
 import com.atguigu.daijia.common.service.RabbitService;
 import com.atguigu.daijia.common.util.RequestUtils;
+import com.atguigu.daijia.driver.client.DriverAccountFeignClient;
 import com.atguigu.daijia.model.entity.payment.PaymentInfo;
+import com.atguigu.daijia.model.enums.TradeType;
+import com.atguigu.daijia.model.form.driver.TransferForm;
 import com.atguigu.daijia.model.form.payment.PaymentInfoForm;
+import com.atguigu.daijia.model.vo.order.OrderRewardVo;
 import com.atguigu.daijia.model.vo.payment.WxPrepayVo;
+import com.atguigu.daijia.order.client.OrderInfoFeignClient;
 import com.atguigu.daijia.payment.config.WxPayV3Properties;
 import com.atguigu.daijia.payment.mapper.PaymentInfoMapper;
 import com.atguigu.daijia.payment.service.WxPayService;
@@ -43,6 +48,11 @@ public class WxPayServiceImpl implements WxPayService {
     @Autowired
     private RabbitService rabbitService;
 
+    @Autowired
+    private OrderInfoFeignClient orderInfoFeignClient;
+
+    @Autowired
+    private DriverAccountFeignClient driverAccountFeignClient;
 
     @Override
     public WxPrepayVo createWxPayment(PaymentInfoForm paymentInfoForm) {
@@ -170,8 +180,19 @@ public class WxPayServiceImpl implements WxPayService {
     @Override
     public void handleOrder(String orderNo) {
         //1 远程调用：更新订单状态：已经支付
+        orderInfoFeignClient.updateOrderPayStatus(orderNo);
 
         //2 远程调用：获取系统奖励，打入到司机账户
+        OrderRewardVo orderRewardVo = orderInfoFeignClient.getOrderRewardFee(orderNo).getData();
+        if(orderRewardVo != null && orderRewardVo.getRewardFee().doubleValue()>0) {
+            TransferForm transferForm = new TransferForm();
+            transferForm.setTradeNo(orderNo);
+            transferForm.setTradeType(TradeType.REWARD.getType());
+            transferForm.setContent(TradeType.REWARD.getContent());
+            transferForm.setAmount(orderRewardVo.getRewardFee());
+            transferForm.setDriverId(orderRewardVo.getDriverId());
+            driverAccountFeignClient.transfer(transferForm);
+        }
 
         //3 TODO 其他
 
