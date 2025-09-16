f894da21a5702a49ac72671dc4f951cedab9f631
action <xingdong1117@126.com>
2025-09-16 14:41:25 +0800

    feat:获取未使用的最佳优惠券信息

--- daijia-parent/service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
+++ daijia-parent/service/service-coupon/src/main/java/com/atguigu/daijia/coupon/service/impl/CouponInfoServiceImpl.java
@@ -9,6 +9,7 @@ import com.atguigu.daijia.coupon.service.CouponInfoService;
 import com.atguigu.daijia.model.entity.coupon.CouponInfo;
 import com.atguigu.daijia.model.entity.coupon.CustomerCoupon;
 import com.atguigu.daijia.model.vo.base.PageVo;
+import com.atguigu.daijia.model.vo.coupon.AvailableCouponVo;
 import com.atguigu.daijia.model.vo.coupon.NoReceiveCouponVo;
 import com.atguigu.daijia.model.vo.coupon.NoUseCouponVo;
 import com.atguigu.daijia.model.vo.coupon.UsedCouponVo;
@@ -20,9 +21,13 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
-import java.util.Date;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
@@ -118,6 +123,78 @@ public class CouponInfoServiceImpl extends ServiceImpl<CouponInfoMapper, CouponI
         }
         return true;
 
+    }
+
+    //获取未使用的最佳优惠卷信息
+    @Override
+    public List<AvailableCouponVo> findAvailableCoupon(Long customerId, BigDecimal orderAmount) {
+
+        //1 创建list集合，存储最终返回数据
+        List<AvailableCouponVo> availableCouponVoList = new ArrayList<>();
+
+        //2 根据乘客id，获取乘客已经领取但是没有使用的优惠卷列表
+        //返回list集合
+        List<NoUseCouponVo> list = couponInfoMapper.findNoUseList(customerId);
+
+        //3 遍历乘客未使用优惠卷列表，得到每个优惠卷
+        //3.1 判断优惠卷类型：现金卷 和 折扣卷
+        List<NoUseCouponVo> typeList = list.stream().filter(item -> item.getCouponType() == 1).collect(Collectors.toList());
+
+        //3.2 是现金券
+        //判断现金卷是否满足条件
+        for(NoUseCouponVo noUseCouponVo:typeList) {
+            //判断使用门槛
+            //减免金额
+            BigDecimal reduceAmount = noUseCouponVo.getAmount();
+            //1 没有门槛  == 0，订单金额必须大于优惠减免金额
+            if(noUseCouponVo.getConditionAmount().doubleValue()==0 && orderAmount.subtract(reduceAmount).doubleValue()>0) {
+                availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo,reduceAmount));
+            }
+
+            //2 有门槛  ，订单金额大于优惠门槛金额
+            if(noUseCouponVo.getConditionAmount().doubleValue() > 0 && orderAmount.subtract(noUseCouponVo.getConditionAmount()).doubleValue()>0) {
+                availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo,reduceAmount));
+            }
+        }
+
+        //3.3 折扣卷
+        //判断折扣卷是否满足条件
+        List<NoUseCouponVo> typeList2 = list.stream().filter(item -> item.getCouponType() == 2).collect(Collectors.toList());
+        for (NoUseCouponVo noUseCouponVo : typeList2) {
+            //折扣之后金额
+            // 100 打8折  = 100 * 8 /10= 80
+            BigDecimal discountAmount = orderAmount.multiply(noUseCouponVo.getDiscount()).divide(new BigDecimal("10")).setScale(2, RoundingMode.HALF_UP);
+
+            BigDecimal reduceAmount = orderAmount.subtract(discountAmount);
+            //2.2.1.没门槛
+            if (noUseCouponVo.getConditionAmount().doubleValue() == 0) {
+                availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo, reduceAmount));
+            }
+            //2.2.2.有门槛，订单折扣后金额大于优惠券门槛金额
+            if (noUseCouponVo.getConditionAmount().doubleValue() > 0
+                    && discountAmount.subtract(noUseCouponVo.getConditionAmount()).doubleValue() > 0) {
+                availableCouponVoList.add(this.buildBestNoUseCouponVo(noUseCouponVo, reduceAmount));
+            }
+
+        }
+
+        //4 把满足条件优惠卷放到最终list集合
+        //根据金额排序
+        if (!CollectionUtils.isEmpty(availableCouponVoList)) {
+            Collections.sort(availableCouponVoList, new Comparator<AvailableCouponVo>() {
+                @Override
+                public int compare(AvailableCouponVo o1, AvailableCouponVo o2) {
+                    return o1.getReduceAmount().compareTo(o2.getReduceAmount());
+                }
+            });
+        }
+
+        return availableCouponVoList;
+    }
+
+    private AvailableCouponVo buildBestNoUseCouponVo(NoUseCouponVo noUseCouponVo, BigDecimal reduceAmount) {
+
+        return null;
     }
 
     private void saveCustomerCoupon(Long customerId, Long couponId, Date expireTime) {
